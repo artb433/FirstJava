50a79a60c78e3559cbcb5bec523d8692dbc06430
Art b <94374097+artb433@users.noreply.github.com>
2021-12-07 11:24:56 +0000

    my fisrt java script

---
+++ JsBasics/index.js
@@ -0,0 +1,13 @@
+let firstname = 'kofi'
+console.log(firstname);
+
+// let age =13;
+// if (age>=18){
+//     console.log('You are an adult')
+// }else {
+//     console.log('You are young')
+// }
+
+let age=34;
+let message=(age=>28)? 'You are an adult':'You are young';
+console.log(message);
